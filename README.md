571db00f93656f778e0dac02465bccad05a0c08d
Rui Zhang <ruizhang@ruis-mbp.lan>
2024-09-08 12:01:13 -0700

    Init commit the simple web browser simulation python code

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # webbrowser
 a simple web browser written in python
+
+python 3 is required to run the code
+
+An example commond to start the browser: 
+
+python browser.py https://wwww.google.com
---
+++ browser.py
@@ -0,0 +1,95 @@
+import tkinter
+import tkinter.font
+import url
+
+WIDTH, HEIGHT = 800, 600
+HSTEP, VSTEP = 13, 18
+SCROLL_STEP = 100
+
+class Browser:
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.bi_times = tkinter.font.Font(
+            family="Times",
+            size=16,
+            weight="bold",
+            slant="italic",
+        )
+        self.canvas = tkinter.Canvas(
+            self.window,
+            width=WIDTH,
+            height=HEIGHT
+        )
+        self.canvas.pack()
+        self.scroll = 0
+        self.window.bind("<Down>", self.scrolldown)
+
+    def draw(self):
+        self.canvas.delete("all")
+        for x, y, c in self.display_list:
+            # skip drawing chars that are offscreen
+            if y > self.scroll + HEIGHT: continue
+            if y + VSTEP < self.scroll: continue
+            self.canvas.create_text(x, y - self.scroll, text=c, font=self.bi_times, anchor='nw')
+
+    def load(self, url):
+        body = url.request()
+        text = lex(body)
+        self.display_list = layout(text)
+        self.draw()
+
+    def scrolldown(self, e):
+        self.scroll += SCROLL_STEP
+        self.draw()
+
+    def scrollup(self, e):
+        self.scroll -= SCROLL_STEP
+        self.draw()
+
+class Text:
+    def __init__(self, text):
+        self.text = text
+
+class Tag:
+    def __init__(self, tag):
+        self.tag = tag
+
+# parse html tag and text
+def lex(body):
+    out = []
+    buffer = ""
+    in_tag = False
+    for c in body:
+        if c == "<":
+            in_tag = True
+            if buffer: out.append(Text(buffer))
+            buffer = ""
+        elif c == ">":
+            in_tag = False
+            out.append(Tag(buffer))
+            buffer = ""
+        else:
+            buffer += c
+    if not in_tag and buffer:
+        out.append(Text(buffer)) 
+    return out
+
+def layout(text):
+    display_list = []
+    cursor_x, cursor_y = HSTEP, VSTEP
+    font = tkinter.font.Font()
+    for word in text.split():
+        w = font.measure(word)
+        display_list.append((cursor_x, cursor_y, word))
+        # move x cursor to the end of the word with one space  
+        cursor_x += w + font.measure(" ")
+        if cursor_x + w >= WIDTH - HSTEP:
+            # adding extra .25 line space, otherwise it is hard to read
+            cursor_y += font.metrics("linespace") * 1.25
+            cursor_x = HSTEP 
+    return display_list
+
+if __name__ == "__main__":
+    import sys
+    Browser().load(url.URL(sys.argv[1]))
+    tkinter.mainloop()
